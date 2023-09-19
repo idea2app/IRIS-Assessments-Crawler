dcb3dd14c0d958617e4eb5e2ebdbfbba3798c9a0
ShaoyaHUANG <shaoyahuang22@gmail.com>
2023-09-20 03:07:49 +0800

    add: sub web page scrapper

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
 #! /usr/bin/env node
 
-import { $ } from 'zx';
+import { $, cd } from 'zx';
+import { promises } from 'fs';
+import { JSDOM } from 'jsdom';
 
 const url = 'https://iris.epa.gov/AtoZ/?list_type=alpha';
-const dirPath = './dist';
 
-async function downloadData() {
-  await $`wget -c -r -npH -k -P ${dirPath} index.html ${url}`;
+await $`rm -rf public`;
+await $`mkdir -p public`;
+cd('public');
+await $`wget -c -r -npH -k -p ${url}`;
+await $`mv AtoZ/index.html\?list_type=alpha AtoZ/index.html`;
+
+const linkLists = await (async () => {
+  const {
+    window: { document },
+  } = await JSDOM.fromFile('AtoZ/index.html');
+  const urls = [
+    ...document.querySelectorAll<HTMLAnchorElement>('tr > td:nth-child(2) > a'),
+  ].map((tag) => {
+    const url = tag.href;
+    tag.setAttribute(
+      'href',
+      url.replace('https://iris.epa.gov/', '/') + '.html'
+    );
+
+    return url;
+  });
+  await promises.writeFile(
+    'AtoZ/index.html',
+    document.documentElement.outerHTML
+  );
+  return urls;
+})();
+
+for (const link of linkLists) {
+  await $`wget -c -r -npH -k -p ${link}`;
+  const path = link.replace('https://iris.epa.gov/', '');
+  await $`mv ${path} ${path}.html`;
 }
-
-downloadData();
