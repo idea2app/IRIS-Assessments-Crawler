b33f032823acaa46531c95faefcc8799b8b4675f
ShaoyaHUANG <shaoyahuang22@gmail.com>
2023-09-19 23:35:47 +0800

    add: use zx to write wget

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-#!/usr/bin/env zx
+#! /usr/bin/env node
+
+import { $ } from 'zx';
+
+const url = 'https://iris.epa.gov/AtoZ/?list_type=alpha';
+const dirPath = './dist';
+
+async function downloadData() {
+  await $`wget -c -r -npH -k -P ${dirPath} --output-document index.html ${url}`;
+}
+
+downloadData();
