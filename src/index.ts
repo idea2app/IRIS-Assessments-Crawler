1321f6f4f6a089b7d735952fa8fc4b0cd621e142
ShaoyaHUANG <shaoyahuang22@gmail.com>
2023-09-19 23:39:41 +0800

    fix: -o can not with -k problem

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const url = 'https://iris.epa.gov/AtoZ/?list_type=alpha';
 const dirPath = './dist';
 
 async function downloadData() {
-  await $`wget -c -r -npH -k -P ${dirPath} --output-document index.html ${url}`;
+  await $`wget -c -r -npH -k -P ${dirPath} index.html ${url}`;
 }
 
 downloadData();
