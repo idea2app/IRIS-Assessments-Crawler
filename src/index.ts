d1fb0547fcbea20c27c0d712656ecbbfc73a22c3
ShaoyaHUANG <shaoyahuang22@gmail.com>
2023-09-20 12:31:42 +0800

    add: download pdfs

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,33 @@
 #! /usr/bin/env node
 
-import { JSDOM } from "jsdom";
-import { $, argv, cd, fs, path } from "zx";
+import { JSDOM } from 'jsdom';
+import { $, argv, cd, fs, path } from 'zx';
 
 const [url, folder] = argv._;
 
 const downloadHTML = (url: string, fixName = false) =>
-  $`wget -c -r -npH -k -p ${fixName ? "-E" : ""} ${url}`;
+  $`wget -c -r -npH -k -p ${fixName ? '-E' : ''} ${url}`;
 
-async function getSubPageURLs(filePath: string, selector: string) {
+async function getExtras(filePath: string, selector: string, isURLs = true) {
   const {
     window: { document },
   } = await JSDOM.fromFile(filePath);
 
-  const urls = [...document.querySelectorAll<HTMLAnchorElement>(selector)].map(
-    (tag) => {
-      const { href, origin } = tag;
+  const elementLists = [
+    ...document.querySelectorAll<HTMLAnchorElement>(selector),
+  ].map((tag) => {
+    const { href, origin } = tag;
 
-      tag.setAttribute("href", href.slice(origin.length) + ".html");
+    tag.setAttribute(
+      'href',
+      isURLs ? href.slice(origin.length) + '.html' : href.slice(origin.length)
+    );
 
-      return href;
-    }
-  );
+    return href;
+  });
   await fs.outputFile(filePath, document.documentElement.outerHTML);
 
-  return urls;
+  return elementLists;
 }
 
 await $`rm -rf ${folder}`;
@@ -36,7 +39,7 @@ const root = new URL(url).pathname.slice(1);
 const files = await fs.readdir(root);
 
 for (const file of files) {
-  const [name, useless] = file.split("?");
+  const [name, useless] = file.split('?');
   const oldName = path.join(root, file),
     newName = path.join(root, name);
 
@@ -44,7 +47,26 @@ for (const file of files) {
 
   if (!/\.html?$/.test(name)) continue;
 
-  const linkList = await getSubPageURLs(newName, "tr > td:nth-child(2) > a");
+  //Get list of PDFs on the 5th column
+  const FirstColumnOfPDFFiles = await getExtras(
+    newName,
+    'tr > td:nth-child(5) > a',
+    false
+  );
+
+  for (const pdf of FirstColumnOfPDFFiles) await downloadHTML(pdf, true);
+
+  //Get list of PDFs on the 6th column
+  const SecondColumnOfPDFFiles = await getExtras(
+    newName,
+    'tr > td:nth-child(6) > a',
+    false
+  );
+
+  for (const pdf of SecondColumnOfPDFFiles) await downloadHTML(pdf, true);
+
+  //Get sub page URLs
+  const linkList = await getExtras(newName, 'tr > td:nth-child(2) > a');
 
   for (const link of linkList) await downloadHTML(link, true);
 }
