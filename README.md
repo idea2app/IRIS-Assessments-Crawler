e7b7c9a8a53ab84f1d0e8f59f87e5d914f1fb90d
Shaoya HUANG <shaoyahuang22@gmail.com>
2023-09-20 23:40:56 +0800

    add: download pdfs (#5)

---
+++ README.md
@@ -0,0 +1,19 @@
+# IRIS-Assessments-Crawler
+
+基于 JSDOM 实现的 Node.js 爬虫工具。
+
+爬取数据来源： https://iris.epa.gov/AtoZ/?list_type=alpha
+
+## 使用方法
+
+1. 在本地运行爬虫脚本
+
+```shell
+pnpm crawl
+```
+
+2. 在本地启动服务器，查看效果
+
+```shell
+pnpm start
+```
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,47 @@
 #! /usr/bin/env node
 
-import { JSDOM } from "jsdom";
-import { $, argv, cd, fs, path } from "zx";
+import { JSDOM } from 'jsdom';
+import { $, argv, cd, fs, path } from 'zx';
 
 const [url, folder] = argv._;
 
 const downloadHTML = (url: string, fixName = false) =>
-  $`wget -c -r -npH -k -p ${fixName ? "-E" : ""} ${url}`;
+  $`wget -c -r -npH -k -p ${fixName ? '-E' : ''} ${url}`;
 
-async function getSubPageURLs(filePath: string, selector: string) {
+async function getExtras(filePath: string, selector: string) {
   const {
     window: { document },
   } = await JSDOM.fromFile(filePath);
 
-  const urls = [...document.querySelectorAll<HTMLAnchorElement>(selector)].map(
-    (tag) => {
-      const { href, origin } = tag;
+  const elementLists = [
+    ...document.querySelectorAll<HTMLAnchorElement>(selector),
+  ].map((tag) => {
+    const { href, origin } = tag;
 
-      tag.setAttribute("href", href.slice(origin.length) + ".html");
+    tag.setAttribute(
+      'href',
+      href.slice(origin.length) + (path.parse(href).ext ? '' : '.html')
+    );
 
-      return href;
-    }
-  );
+    return href;
+  });
   await fs.outputFile(filePath, document.documentElement.outerHTML);
 
-  return urls;
+  return elementLists;
+}
+
+async function modifySubPageURLs(filePath: string, selector: string) {
+  const {
+    window: { document },
+  } = await JSDOM.fromFile(filePath);
+
+  for (const tag of document.querySelectorAll<HTMLAnchorElement>(selector)) {
+    const { href, origin } = tag;
+
+    tag.setAttribute('href', href.slice(origin.length));
+  }
+
+  await fs.outputFile(filePath, document.documentElement.outerHTML);
 }
 
 await $`rm -rf ${folder}`;
@@ -36,7 +53,7 @@ const root = new URL(url).pathname.slice(1);
 const files = await fs.readdir(root);
 
 for (const file of files) {
-  const [name, useless] = file.split("?");
+  const [name, useless] = file.split('?');
   const oldName = path.join(root, file),
     newName = path.join(root, name);
 
@@ -44,7 +61,13 @@ for (const file of files) {
 
   if (!/\.html?$/.test(name)) continue;
 
-  const linkList = await getSubPageURLs(newName, "tr > td:nth-child(2) > a");
+  const linkList = await getExtras(newName, 'tr > td > a');
 
   for (const link of linkList) await downloadHTML(link, true);
 }
+
+const subPageFiles = await fs.readdir('ChemicalLanding');
+
+cd('ChemicalLanding');
+
+for (const file of subPageFiles) await modifySubPageURLs(file, 'li > .under');
